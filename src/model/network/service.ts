44cd502b535ef2668af815bcf116a77d7d1302a7
pdmuds4 <ryosuke3191@gmail.com>
2024-07-08 12:26:01 +0900

    proxy->ダイレクトに変更

--- src/model/network/service.ts
+++ src/model/network/service.ts
@@ -6,7 +6,7 @@ import { callAPI } from "@utils/callAPI";
 export default class NetworkDomainService {
     async postToApi(request: RequestEntity): Promise<NetworkResponse> {
         return await callAPI<NetworkRequest, NetworkResponse>(
-            "http://localhost:5173/api/bays/network",
+            "/api/bays/network",
             "POST",
             request.json
         );
